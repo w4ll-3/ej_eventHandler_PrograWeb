809a582ee1f722ffc47deb77e7d822a3fee40f52
Wellington Cua <wjcua@correo.url.edu.gt>
2024-04-18 11:38:11 -0600

    clase 18042024 hooks useState, manejo de estados

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,47 @@
 "use client";
 
-import { AlertButton, LogButton } from "@/components";
-import { MouseEvent } from "react";
+import { ChangeEvent, useState } from 'react';
+import Button from '../components/Button';
 
 export default function Home() {
-  const handleClick = (event: MouseEvent<HTMLDivElement>) => {
-    console.log("Hola!")
-    console.log(event.target)
+  const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+
+  // setInterval(() => {
+  //   setCount((prevState) => prevState + 1);
+  // }, 100);
+
+  const handleIncrease = () => {
+    console.log('Incrementar')
+    setCount((prevState) => {
+      return prevState + 1;
+    });
+  };
+
+  const handleDecrease = () => {
+    console.log('Decrementar')
+    setCount((prevState) => prevState - 1);
   }
+
+  const handleReset = () => {
+    console.log('Reiniciar')
+    setCount(0);
+  }
+
+  const handleChance = (event: ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  }
+
   return (
     <main className="flex flex-col items-start gap-4">
       <h1 className="text-5xl">Event Handlers</h1>
-      <div onClick={handleClick} className="bg-gray-500 p-8 flex gap-4">
-        <AlertButton message="hola!">Saludar</AlertButton>
-        <LogButton message="adios!">Despedirse</LogButton>
+      <p>{count}</p>
+      <div className="flex gap-4">
+        <Button onClick={handleIncrease}>Incrementar</Button>
+        <Button onClick={handleDecrease}>Incrementar</Button>
+        <Button onClick={handleReset}>Reiniciar</Button>
       </div>
+      <input className='text-black' onChange={handleChance} type="text"/>
     </main>
   )
 }
